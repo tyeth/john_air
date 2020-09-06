3958b53c81e5cfa1e0e032e41541734388af46c0
Tyeth Gundry <tyethgundry@googlemail.com>
2020-09-06 16:40:52 +0100

    Added AQI conversion functions

--- main.py
+++ main.py
@@ -20,13 +20,31 @@ Example sketch to connect to PM2.5 sensor with either I2C or UART.
 # pylint: disable=unused-import
 import time
 import subprocess
-
+import blynklib
 import board
 import busio
 import serial
 from digitalio import DigitalInOut, Direction, Pull
 import adafruit_pm25
 import adafruit_si7021
+
+i2c=None
+lcd=None
+sensor=None
+uart=None
+pm25=None
+currentValues=None
+lcdString=None
+
+def updateLCD(newString):
+    print("UpdateLCD called with %s" % newString)
+    global lcdString,lcd
+    if(newString==lcdString): return
+    print("Updating LCD from %s to %s" % (lcdString,newString))
+    lcdString = newString
+    lcd.clear()
+    lcd.cursor_pos(0)
+    lcd.write_string(lcdString)
 
 
 def diskSpace():
@@ -36,16 +54,9 @@ def diskSpace():
                      universal_newlines=True)
     stdout, stderr = process.communicate()
     disk_free = stdout.split('\n')[1].split()[3]
-    print("Free Space:")
-    print(disk_free)
+    outString = "Free Space:\n%s" % disk_free
+    updateLCD(outString)
     time.sleep(2)
-
-i2c=None
-sensor=None
-uart=None
-pm25=None
-currentValues=None
-lcdString=None
 
 def doPmReading(pm25):
     if(pm25==None):
@@ -80,6 +91,113 @@ def doPmReading(pm25):
         print("---------------------------------------")
     except RuntimeError:
         print("Unable to read from sensor, retrying on next round...")
+
+    try:
+        print("Updating blynk with PPM...")
+        blynk.set_property(pin_PM25,'color', getHexColor(aqdata) )
+    except expression as identifier:
+        pass
+
+def  calcAQIpm10(pm10):
+	pm1 = 0
+	pm2 = 54
+	pm3 = 154
+	pm4 = 254
+	pm5 = 354
+	pm6 = 424
+	pm7 = 504
+	pm8 = 604
+	aqi1 = 0
+	aqi2 = 50
+	aqi3 = 100
+	aqi4 = 150
+	aqi5 = 200
+	aqi6 = 300
+	aqi7 = 400
+	aqi8 = 500
+	aqipm10 = 0
+
+	if (pm10 >= pm1 & pm10 <= pm2) :
+		aqipm10 = ((aqi2 - aqi1) / (pm2 - pm1)) * (pm10 - pm1) + aqi1
+	elif (pm10 >= pm2 & pm10 <= pm3) :
+		aqipm10 = ((aqi3 - aqi2) / (pm3 - pm2)) * (pm10 - pm2) + aqi2
+	elif (pm10 >= pm3 & pm10 <= pm4) :
+		aqipm10 = ((aqi4 - aqi3) / (pm4 - pm3)) * (pm10 - pm3) + aqi3
+	elif (pm10 >= pm4 & pm10 <= pm5) :
+		aqipm10 = ((aqi5 - aqi4) / (pm5 - pm4)) * (pm10 - pm4) + aqi4
+	elif (pm10 >= pm5 & pm10 <= pm6) :
+		aqipm10 = ((aqi6 - aqi5) / (pm6 - pm5)) * (pm10 - pm5) + aqi5
+	elif (pm10 >= pm6 & pm10 <= pm7): 
+		aqipm10 = ((aqi7 - aqi6) / (pm7 - pm6)) * (pm10 - pm6) + aqi6
+	elif (pm10 >= pm7 & pm10 <= pm8) :
+		aqipm10 = ((aqi8 - aqi7) / (pm8 - pm7)) * (pm10 - pm7) + aqi7
+	elif (pm10 > pm8) :
+		aqipm10 = 500
+	
+	return aqipm10.toFixed(0)
+
+
+
+	# https://www.airnow.gov/sites/default/files/2020-05/aqi-technical-assistance-document-sept2018.pdf 
+def getColor(aqi) :
+    color=None
+    if (aqi < 50):
+        color = "Lime"
+    elif (aqi >= 50 & aqi < 100):
+        color = "yellow"
+    elif (aqi >= 100 & aqi < 150):
+        color = "orange"
+    elif (aqi >= 150 & aqi < 200):
+        color = "red"
+    elif (aqi >= 200 & aqi < 300):
+        color = "purple"
+    elif (aqi >= 300):
+        color = "rgb(126,0,35)" #/* was brown, should be maroon, rgb(126,0,35) */
+    else:
+        color = "black"
+    
+	return {"bg": color, "text": "white" if (aqi > 200) else "black"} 
+
+
+def calcAQIpm25(pm25):
+    pm1 = 0
+    pm2 = 12
+    pm3 = 35.4
+    pm4 = 55.4
+    pm5 = 150.4
+    pm6 = 250.4
+    pm7 = 350.4
+    pm8 = 500.4
+
+    aqi1 = 0
+    aqi2 = 50
+    aqi3 = 100
+    aqi4 = 150
+    aqi5 = 200
+    aqi6 = 300
+    aqi7 = 400
+    aqi8 = 500
+
+    aqipm25 = 0
+
+    if (pm25 >= pm1 & pm25 <= pm2):
+        aqipm25 = ((aqi2 - aqi1) / (pm2 - pm1)) * (pm25 - pm1) + aqi1
+    elif (pm25 >= pm2 & pm25 <= pm3):
+        aqipm25 = ((aqi3 - aqi2) / (pm3 - pm2)) * (pm25 - pm2) + aqi2
+    elif (pm25 >= pm3 & pm25 <= pm4):
+        aqipm25 = ((aqi4 - aqi3) / (pm4 - pm3)) * (pm25 - pm3) + aqi3
+    elif (pm25 >= pm4 & pm25 <= pm5):
+        aqipm25 = ((aqi5 - aqi4) / (pm5 - pm4)) * (pm25 - pm4) + aqi4
+    elif (pm25 >= pm5 & pm25 <= pm6):
+        aqipm25 = ((aqi6 - aqi5) / (pm6 - pm5)) * (pm25 - pm5) + aqi5
+    elif (pm25 >= pm6 & pm25 <= pm7):
+        aqipm25 = ((aqi7 - aqi6) / (pm7 - pm6)) * (pm25 - pm6) + aqi6
+    elif (pm25 >= pm7 & pm25 <= pm8):
+        aqipm25 = ((aqi8 - aqi7) / (pm8 - pm7)) * (pm25 - pm7) + aqi7
+    elif (pm25 > pm8):
+        aqipm25 = 500
+
+	return aqipm25.toFixed(0)
 
 def doTemperatureHumidityReading(sensor):
     if(sensor==None):
